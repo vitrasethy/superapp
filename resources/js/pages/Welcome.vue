4681ef7a82222d42b11c1567f12ccc92c282a4bd
vitrasethy <sethy.vitra@gmail.com>
2025-11-11 11:46:32 +0700

    commit

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -13,7 +13,7 @@ withDefaults(
 </script>
 
 <template>
-    <Head title="Welcome">
+    <Head title="Welcome!">
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
